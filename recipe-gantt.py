3dcbf78167a98d239d6d0d428729ad488aaeb75b
jbremz <jhbremner@gmail.com>
2024-01-12 15:45:23 +0000

    tidy up

--- recipe-gantt.py
+++ recipe-gantt.py
@@ -1,4 +1,22 @@
+import logging
+from io import StringIO
+
+import pandas as pd
+import plotly.graph_objects as go
+from fire import Fire
+from llama_cpp import Llama
 from recipe_scrapers import scrape_me
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.INFO)
+c_format = logging.Formatter("🧑‍🍳 RECIPE GANTT: %(message)s")
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
+logger.propagate = False
+
 
 INSTRUCTION = """Your task is to transform cooking recipes from raw text into a Gantt chart .tsv file which conveys all the same information but graphically so one can see which ingredients are involved in each step. In the end, we wish to produce a downloadable .tsv file containing a table. It will be structured as follows:
 
@@ -109,3 +127,97 @@ def get_alpaca_prompt(record):
 {record['input']}
 
 ### Response:"""
+
+
+def get_prompt(url):
+    """
+    Retrieves a prompt for generating text based on a given URL.
+
+    Args:
+        url (str): The URL of the recipe.
+
+    Returns:
+        str: The generated prompt for text generation.
+    """
+    recipe = get_recipe(url)
+    record = {}
+    record["instruction"] = INSTRUCTION
+    record["input"] = recipe
+    prompt = get_alpaca_prompt(record)
+    return prompt
+
+
+def read_tsv(output):
+    """
+    Read a tab-separated values (TSV) file from a string and return a pandas DataFrame.
+
+    Parameters:
+    output (str): The TSV file content as a string.
+
+    Returns:
+    pandas.DataFrame: The DataFrame containing the parsed TSV data.
+    """
+    return pd.read_csv(StringIO(output), sep="\t")
+
+
+def plot_gantt(df, figsize=(1000, 2000)):
+    """
+    Plot a Gantt chart using the provided DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the data to be plotted.
+        figsize (tuple, optional): The size of the figure in pixels. Defaults to (1500, 2000).
+    """
+    pdf = df.fillna("")
+    pdf = pdf.replace("X", "✓")
+
+    fig = go.Figure(
+        data=[
+            go.Table(
+                header=dict(
+                    values=list(pdf.columns), fill_color="blanchedalmond", align="left"
+                ),
+                cells=dict(
+                    values=[pdf[col] for col in pdf.columns],
+                    fill_color="floralwhite",
+                    align="center",
+                ),
+            )
+        ]
+    )
+
+    fig.update_layout(autosize=False, width=figsize[0], height=figsize[1])
+
+    fig.show()
+
+
+def main(
+    output_path="recipe-gantt.tsv",
+    display=True,
+    model_path="./models/merged_ggml-model-q4_0.gguf",
+):
+    logger.info("Loading model...")
+    llm = Llama(
+        model_path=model_path,
+        n_ctx=0,  # default length which I think is 8192
+        n_gpu_layers=1,  # use M1 metal
+        verbose=False,
+    )
+    url = input("🧑‍🍳 Please enter the recipe URL: ")
+    logger.info("Downloading recipe ⬇️")
+    prompt = get_prompt(url)
+    logger.info("Generating gantt chart...")
+    output = llm(prompt, max_tokens=4096, echo=False)
+    output = output["choices"][0]["text"]
+    with open(output_path, "w") as file:
+        file.write(output)
+    logger.info(f"Written recipe gantt chart to {output_path}")
+
+    if display:
+        logger.info("Opening recipe gantt chart for display 🖥️")
+        df = read_tsv(output)
+        plot_gantt(df)
+
+
+if __name__ == "__main__":
+    Fire(main)
